8273f811d5dcb8f11ec416b0d3dd4380291d2165
ericandrewlewis <eric.andrew.lewis@gmail.com>
2011-09-29 16:45:38 -0300

    first draft of pull() and push() functionality for Users.

--- libs/php/User.php
+++ libs/php/User.php
@@ -24,6 +24,45 @@ class User {
         assert('$domain_key != null /* Domain key is necessary to generate token */');
         return Token::from_user($this);
     }
+    
+    public function pull( $user_data ) {
+        extract( $user_data );
+
+        $token_base64 = base64( $this->token() );
+        $domain = $this->get_domain( );
+        $pull_profile_url = "http://{$domain}/users/get_remote_profile?id={$user_id}";
+        $pull_profile_url_urlencoded = urlencode( $pull_profile_url );
+        $remote_url = "http://{$domain}/?actor_token={$token_base64}&pull_profile_url={$pull_profile_url_urlencoded}";
+
+        $ch=curl_init($remote_url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        return $response;
+    }
+
+    public function push( $user_data ) {
+        $user_data_in_json_format = json_encode($user_data);
+        $token_base64 = base64( $this->token() );
+        $domain = $this->get_domain( );
+        $remote_url = "http://{$domain}/profiles/?actor_token={$token_base64}&id={$user_data['user_id']}";
+
+        $ch=curl_init($remote_url);
+        
+        $curl_options = array(
+            'CURLOPT_POST'              => true,
+            'CURLOPT_POSTFIELDS'        => $user_data_in_json_format,
+            'CURLOPT_RETURNTRANSFER'    => true,
+            'CURLOPT_HTTPHEADER'        => array('Content-Type: application/json')
+        );
+
+        curl_setopt_array( $ch, $curl_options );
+        $response = curl_exec( $ch );
+        curl_close( $ch );
+
+        return $response;
+    }
 }
 
 ?>
