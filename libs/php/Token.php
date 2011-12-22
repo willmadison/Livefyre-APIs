2cfb38aa80dba4453c4fb0f3186afe1ea4e4ed70
Ross Pfahler <ross.pfahler@gmail.com>
2011-12-21 16:35:36 -0800

    correction for proper expiration name

--- libs/php/Token.php
+++ libs/php/Token.php
@@ -8,7 +8,7 @@ define('TOKEN_EXPIRATION', time() + 86400);
 class Token {
     static function from_user($user, $token_expiration=TOKEN_EXPIRATION) {
         $key = $user->get_domain()->get_key();
-        $token = array('domain'=>$user->get_domain()->get_host(), 'user_id'=>$user->get_uid(), 'expiration'=>$token_expiration);
+        $token = array('domain'=>$user->get_domain()->get_host(), 'user_id'=>$user->get_uid(), 'expires'=>$token_expiration);
         return JWT.decode($token, $key);
     }
 }
