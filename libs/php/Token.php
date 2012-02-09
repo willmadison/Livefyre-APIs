1fa14f758357ccb649587ea5e119c4248ae72e99
Chris <chris@livefyre.com>
2012-02-09 01:53:02 -0800

    remove tabs, replace, we use spaces

--- libs/php/Token.php
+++ libs/php/Token.php
@@ -13,73 +13,72 @@ class Livefyre_Token {
 }
 
 function getHmacsha1Signature($key, $data) {
-    	//convert binary hash to BASE64 string
-    	return base64_encode(hmacsha1($key, $data));
+        //convert binary hash to BASE64 string
+    return base64_encode(hmacsha1($key, $data));
 }
 
 // encrypt a base string w/ HMAC-SHA1 algorithm
 function hmacsha1($key,$data) {
-    	$blocksize=64;
-    	$hashfunc='sha1';
-    	if (strlen($key)>$blocksize) {
-            	$key=pack('H*', $hashfunc($key));
-    	}
-    	$key=str_pad($key,$blocksize,chr(0x00));
-    	$ipad=str_repeat(chr(0x36),$blocksize);
-    	$opad=str_repeat(chr(0x5c),$blocksize);
-    	$hmac = pack( 'H*',$hashfunc( ($key^$opad).pack( 'H*',$hashfunc( ($key^$ipad).$data ) ) ) );
-    	return $hmac;
+    $blocksize=64;
+    $hashfunc='sha1';
+    if (strlen($key)>$blocksize) {
+        $key=pack('H*', $hashfunc($key));
+    }
+    $key=str_pad($key,$blocksize,chr(0x00));
+    $ipad=str_repeat(chr(0x36),$blocksize);
+    $opad=str_repeat(chr(0x5c),$blocksize);
+    $hmac = pack( 'H*',$hashfunc( ($key^$opad).pack( 'H*',$hashfunc( ($key^$ipad).$data ) ) ) );
+    return $hmac;
 }
 
 function xor_these($first, $second) {
-    	$results=array();
-   for ($i=0; $i < strlen($first); $i++)
-   {
-            	array_push($results, $first[$i]^$second[$i]);
-   }
-   return implode($results);
+    $results=array();
+    for ($i=0; $i < strlen($first); $i++) {
+        array_push($results, $first[$i]^$second[$i]);
+    }
+    return implode($results);
 }
 
 function Livefyre_hasNoComma($str) {
-    	return !preg_match('/\,/', $str);
+    return !preg_match('/\,/', $str);
 }
 
 function lftokenCreateData($now, $duration, $args=array()) {
-    	//Create the right data input for Livefyre authorization
-    	$filtered_args = array_filter($args, 'Livefyre_hasNoComma');
-    	if (count($filtered_args)==0 or count($args)>count($filtered_args)) {
-            	return -1;
-    	}
+    //Create the right data input for Livefyre authorization
+    $filtered_args = array_filter($args, 'Livefyre_hasNoComma');
+    if (count($filtered_args)==0 or count($args)>count($filtered_args)) {
+        return -1;
+    }
 
-    	array_unshift($filtered_args, "lftoken", $now, $duration);
-    	$data=implode(',',$filtered_args);
-    	return $data;
+    array_unshift($filtered_args, "lftoken", $now, $duration);
+    $data=implode(',',$filtered_args);
+    return $data;
 }
 
 function lftokenCreateToken($data, $key) {
-    	//Create a signed token from data
-    	$clientkey = hmacsha1($key,"Client Key");
-    	$clientkey_sha1 = sha1($clientkey, true);
-    	$temp = hmacsha1($clientkey_sha1,$data);
-    	$sig = xor_these($temp,$clientkey);
-    	$base64sig = base64_encode($sig);
-    	return base64_encode(implode(",",array($data,$base64sig)));
+    //Create a signed token from data
+    $clientkey = hmacsha1($key,"Client Key");
+    $clientkey_sha1 = sha1($clientkey, true);
+    $temp = hmacsha1($clientkey_sha1,$data);
+    $sig = xor_these($temp,$clientkey);
+    $base64sig = base64_encode($sig);
+    return base64_encode(implode(",",array($data,$base64sig)));
 }
 
 function lftokenValidateServerToken($token, $key) {
-	$parts = explode( ',', $server_token );
-	$signature = array_pop( $parts );
-	$serverkey = hmacsha1( base64_decode( $key ), "Server Key" );
-	$temp = base64_encode( hmacsha1( $serverkey, implode( ',', $parts ) ) );
+    $parts = explode( ',', $server_token );
+    $signature = array_pop( $parts );
+    $serverkey = hmacsha1( base64_decode( $key ), "Server Key" );
+    $temp = base64_encode( hmacsha1( $serverkey, implode( ',', $parts ) ) );
     $timestamp = $parts[1];
     $duration = $parts[2];
-	return ( $signature == $temp ) && ( time() - strtotime( $timestamp ) < $duration );
+    return ( $signature == $temp ) && ( time() - strtotime( $timestamp ) < $duration );
 }
 
 function lftokenValidateResponse($data, $response, $key) {
-	// This was a poorly chosen name, not a great interface.
-	// Deprecated but here for backcompat
-	return lftokenValidateServerToken($data . ',' . $response, $key);
+    // This was a poorly chosen name, not a great interface.
+    // Deprecated but here for backcompat
+    return lftokenValidateServerToken($data . ',' . $response, $key);
 }
 
 /*
